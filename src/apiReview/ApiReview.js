f34319145d4f553a9e3f5c20d5e76399be08f40a
Mohammad Hamzah Iqbal <mohammad.iqbal.7@city.ac.uk>
2022-05-02 22:28:06 +0100

    changed ApiReview to class

--- src/apiReview/ApiReview.js
+++ src/apiReview/ApiReview.js
@@ -1,65 +1,65 @@
 import './ApiReview.css';
-import React, {useState } from 'react';
+import React, { Component, useState } from 'react';
 
 //using axios to make api request 
 import axios from 'axios';
 
-function ApiReview(){
+class ApiReview extends Component {
 
-    
-    const fetchBtcPrice ='https://api.coinbase.com/v2/prices/spot?currency=USD'
+    constructor(props) {
+        super(props);
+        this.fetchBtcPrice = 'https://api.coinbase.com/v2/prices/spot?currency=USD';
+        //this is a object 
+        this.state={
+            price:null
+        }
+        this.fetchBtc()
+    }
+
     //by default it is null
     //get btc price 
-    const [price, updatePrice] = useState(null)
 
-    
-        
-    async function fetchprice() {
+    fetchBtc = async () => {
         try {
-          const response = await axios.get(fetchBtcPrice);
-          console.log(response.data);
-          updatePrice(response.data);
+            const response = await axios.get(this.fetchBtcPrice);
+            console.log(response.data);
+            this.setState({
+                price:response.data
+            })
         } catch (error) {
-          console.error(error);
+            console.error(error);
         }
-        
-
-      }
-      
-
-    //CALL Bitcoin function 
-    function updateBtc(){
-        fetchprice();
     }
+
     //price will first start of with null so it would call the other return 
-    if(price){
-        return (
+    render() {
+        if (this.state.price) {
+            return(
             <>
                 <div className='review'>
-                <h1>BTC-USD Price </h1>
-                    <h1>{price.data.amount}</h1>
+                    <h1>BTC-USD Price </h1>
+                    <h1>{this.state.price.data.amount}</h1>
                     <i class="fas fa-band-aid"></i>
                     <p>This is the current bitcoin price it was fetched using coinbases api and uses axios library to fetch the request.</p>
                     <p>Refresh the page to update the price. Im working on changing the color.</p>
                 </div>
-        
-    
+            </>);
+        //just in case data cant be fetched 
+        } else{
+            return(
+            <>
+                <div className='review'>
+                    <h1>API Request error</h1>
+                    <p>If you are reading this most likely I have reached the limit of my API requests or my code failed</p>
+                </div>
             </>
-        );
+            );
+        }
+
     }
     //this is returned first - update function is called 
-    return(
-        <>
-            <div className='review'>
-                <h1>API Request error</h1>
-                <p>If you are reading this most likely I have reached the limit of my API requests or my code failed</p>
-            
-                {updateBtc()}
-                
-            </div>
-        </>
-    );
-        
+
+
 }
 
 export default ApiReview
